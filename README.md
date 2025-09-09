b8750e1f9c6414c2dc138472d8a643f791878bcd
Kristofer Lundgren <kristofer.lundgren@cgi.com>
2025-09-09 23:13:07 +0200

    Added devbox and devenv integration.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Argo-CD deployment
 
 This demonstrates a quick initial deployment of ArgoCD in the "argocd" namespace, where ArgoCD is also managing it's own deployment as an ArgoCD Application. This makes it possible for ArgoCD to upgrade itself.
+
+```shell
+./install-argocd.sh
+```
--- install-argocd.sh
+++ install-argocd.sh
@@ -4,10 +4,10 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update argo
 helm search repo argo/argo-cd --versions | head -6
 
-ARGOCD_VERSION=$(grep "targetRevision:" application.yaml | tr -cd "[:digit:].")
+ARGOCD_VERSION=$(grep "targetRevision:" templates/application.yaml | tr -cd "[:digit:].")
 helm upgrade --install argocd --create-namespace -n argocd argo/argo-cd --version ${ARGOCD_VERSION}
 
-kubectl apply -k .
+kubectl apply -k templates
 kubens argocd
 argocd login --core
 argocd app list
