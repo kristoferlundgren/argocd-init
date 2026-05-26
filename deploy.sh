4f91e754853f249e31a10bf09d57d040f5e5b3b6
Kristofer Lundgren <kristofer@famlundgren.org>
2026-05-27 00:07:49 +0200

    Use "helm template" to deploy ArgoCD

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,10 @@ helm repo update argo
 helm search repo argo/argo-cd --versions | head -6
 
 # Install ArgoCD:
+kubectl create namespace argocd
 ARGOCD_VERSION=$(grep "targetRevision:" templates/application.yaml | tr -cd "[:digit:].")
-helm upgrade --install argocd --create-namespace -n argocd argo/argo-cd --version ${ARGOCD_VERSION}
+helm template argocd --create-namespace -n argocd argo/argo-cd --version ${ARGOCD_VERSION} \
+  | kubectl apply -f -
 kubectl apply -k templates
 
 # Connect to and show status:
